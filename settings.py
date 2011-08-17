d2b37beb3393be8d958abce7686d93194af59ba4
Qi <Qi@localhost>
2011-08-17 13:12:38 +0800

    change default user_agent

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ __doc__ = 'default application wide settings'
 import logging
 
 cache_file = '.cache.db' # file to use for pdict cache
-user_agent = 'Mozilla/5.0 (X11; U; Linux; de-DE) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.8.0' # user agent for HTTP requests
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0' # user agent for HTTP requests
 log_level = logging.DEBUG # logging level
 log_file = 'webscraping.log' # default logging file

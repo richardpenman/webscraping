627ef59d90757f4814374ba8d95e255d9c126ab3
Richard Baron Penman <Richard Baron Penman@localhost>
2011-10-13 17:00:27 +0900

    added setup script for installers

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(
+    name='webscraping', 
+    py_modules=['__init__', 'adt', 'alg', 'common', 'download', 'pdict', 'settings', 'webkit', 'xpath'],
+    url='http://code.google.com/p/webscraping'
+)

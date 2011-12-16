8bf5befe5303edc34fcd6f880ac414512a46e80f
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2011-12-17 01:22:43 +0900

    removed setup.py, which was not installing as people expect

--- setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-setup(
-    name='webscraping', 
-    py_modules=['__init__', 'adt', 'alg', 'common', 'download', 'pdict', 'settings', 'webkit', 'xpath'],
-    url='http://code.google.com/p/webscraping'
-)

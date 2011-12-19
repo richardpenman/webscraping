c02e35ab9e06be9464861e25706c600377c2c20e
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2011-12-19 13:02:21 +0900

    added details to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup
 setup(
     name='webscraping', 
+    version='1',
     packages=['webscraping'],
     package_dir={'webscraping':'.'}, # look for package contents in current directory
-    url='http://code.google.com/p/webscraping'
+    author='Richard Penman',
+    author_email='richard@sitescraper.net',
+    description='Pure python library aimed to make web scraping easier',
+    url='http://code.google.com/p/webscraping',
+    license='lgpl',
 )

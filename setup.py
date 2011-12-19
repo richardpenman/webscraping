b23483780c6300de6a6bbf9b6d4fb7e3aa7d8014
Richard Baron Penman <Richard Baron Penman@localhost>
2011-12-19 12:56:09 +0900

    added setup.py that will install to proper package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(
+    name='webscraping', 
+    packages=['webscraping'],
+    package_dir={'webscraping':'.'}, # look for package contents in current directory
+    url='http://code.google.com/p/webscraping'
+)
